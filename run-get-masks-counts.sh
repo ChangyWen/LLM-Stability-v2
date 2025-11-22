cccde523257f6cc74f3263fcfd76633fbaaef90a
ChangyWen <ddcwen@gmail.com>
2025-11-22 20:36:21 +0800

    chore: new run

---
+++ run-get-masks-counts.sh
@@ -0,0 +1,11 @@
+set -x
+
+dataset=$1
+total_count=$2
+file_name=$3
+
+
+for ((i=0; i<total_count; i++)); do
+    nohup python src/$dataset/get-masks-counts.py $total_count $i $file_name >/dev/null 2>&1 &
+    sleep 0.1
+done
