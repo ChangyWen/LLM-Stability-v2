54449b08c1c9bccf088d26ed3a6f1cfc94fa9a41
ChangyWen <ddcwen@gmail.com>
2025-11-22 08:40:31 +0800

    chore: new run

--- src/medmcqa/get-masks-completion.py
+++ src/medmcqa/get-masks-completion.py
@@ -161,7 +161,7 @@ if __name__ == "__main__":
             "original_response": original_responses[i],
             "partial_response": partial_responses[i],
             "ground_truth": ground_truths[i],
-            "response": text_output,
+            "responses": [text_output],
             "token_ids": token_ids,
             "logprobs": logprobs,
             "length": len(token_ids),
