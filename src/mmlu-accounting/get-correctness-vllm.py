6a8f6a165c4c5caaa8aac34c59f399acc2ded633
ChangyWen <ddcwen@gmail.com>
2026-04-01 10:39:41 +0000

    chore: new run

--- src/mmlu-accounting/get-correctness-vllm.py
+++ src/mmlu-accounting/get-correctness-vllm.py
@@ -103,24 +103,13 @@ def load_dataset(file, tokenizer):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, required=False, default="Qwen/Qwen3-30B-A3B-Thinking-2507")
+    parser.add_argument("--model_name", type=str, required=False, default="Qwen/Qwen3-235B-A22B-Thinking-2507")
     parser.add_argument("--tensor_parallel_size", type=int, required=False, default=8)
     parser.add_argument("--max_prompt_length", type=int, required=False, default=10240)
     parser.add_argument("--max_completion_length", type=int, required=False, default=10240)
     parser.add_argument("--temperature", type=float, required=False, default=0.6)
-    parser.add_argument("--file_name", type=str, required=True)
+    parser.add_argument("--file_name", type=str, required=False)
     args = parser.parse_args()
-
-    file_name = args.file_name + "_counts.jsonl"
-    all_files = os.listdir("/mnt/blob_output/v-dachengwen/LLM-Stability-v2/backup-from-sandbox-2026-0315-2324/outputs/mmlu-accounting/processed_results")
-    assert file_name in all_files
-
-    save_dir = "/mnt/blob_output/v-dachengwen/LLM-Stability-v2/outputs/mmlu-accounting/processed_results"
-    save_file = os.path.join(save_dir, file_name.replace("_counts.jsonl", "_correctness-vllm.jsonl"))
-
-    if os.path.exists(save_file):
-        print(f"Results already exist in [{save_file}], skipping ...")
-        exit()
 
     llm = LLM(
         model=args.model_name,
@@ -131,35 +120,60 @@ if __name__ == "__main__":
         tensor_parallel_size=args.tensor_parallel_size,
         trust_remote_code=True,
     )
-
-    prompts, questions, answers, ground_truths, uuids, idxs = load_dataset(file_name, llm.get_tokenizer())
-
-    print(f"Processing [{args.model_name}] on [{file_name}] ({len(prompts)} prompts) ...")
-    print(f"Results will be saved to [{save_file}] ...")
-
     sampling_params = SamplingParams(
         n=1,
         temperature=args.temperature,
         top_p=0.95,
         max_tokens=args.max_completion_length,
     )
-    outputs = llm.generate(prompts, sampling_params, use_tqdm=True)
 
-    res = []
-    for i in range(len(outputs)):
-        assert len(outputs[i].outputs) == 1, f"Expected {1} outputs, but got {len(outputs[i].outputs)}"
-        response = outputs[i].outputs[0].text.strip()
-        correctness = extract_from_tags(remove_thinking_draft(response), "correctness")
-        res.append({
-            "idx": idxs[i],
-            "uuid": uuids[i],
-            "question": questions[i],
-            "answer": answers[i],
-            "ground_truth": ground_truths[i],
-            "correctness": correctness,
-        })
+    for file_name in [
+        "Qwen3-4B_temp0.6_n50",
+        "Qwen3-4B_temp0.6_n50_dt",
+        "Qwen3-32B_temp0.6_n50",
+        "Qwen3-32B_temp0.6_n50_dt",
+        "Qwen3-30B-A3B_temp0.6_n50",
+        "Qwen3-30B-A3B_temp0.6_n50_dt",
+        "Seed-OSS-36B-Instruct_temp1.1_n50",
+        "Seed-OSS-36B-Instruct_temp1.1_n50_dt",
+        "NVIDIA-Nemotron-Nano-9B-v2_temp0.6_n50",
+        "NVIDIA-Nemotron-Nano-9B-v2_temp0.6_n50_dt",
+        "NVIDIA-Nemotron-Nano-12B-v2_temp0.6_n50",
+        "NVIDIA-Nemotron-Nano-12B-v2_temp0.6_n50_dt",
+    ]:
+        file_name = file_name + "_counts.jsonl"
+        all_files = os.listdir("/mnt/blob_output/v-dachengwen/LLM-Stability-v2/backup-from-sandbox-2026-0315-2324/outputs/mmlu-accounting/processed_results")
+        assert file_name in all_files
 
-    # save to jsonl file
-    with open(save_file, "a") as f:
-        for item in res:
-            f.write(json.dumps(item) + "\n")
+        save_dir = "/mnt/blob_output/v-dachengwen/LLM-Stability-v2/outputs/mmlu-accounting/processed_results"
+        save_file = os.path.join(save_dir, file_name.replace("_counts.jsonl", "_correctness-vllm-v2.jsonl"))
+
+        if os.path.exists(save_file):
+            print(f"Results already exist in [{save_file}], skipping ...")
+            continue
+
+        prompts, questions, answers, ground_truths, uuids, idxs = load_dataset(file_name, llm.get_tokenizer())
+
+        print(f"Processing [{args.model_name}] on [{file_name}] ({len(prompts)} prompts) ...")
+        print(f"Results will be saved to [{save_file}] ...")
+
+        outputs = llm.generate(prompts, sampling_params, use_tqdm=True)
+
+        res = []
+        for i in range(len(outputs)):
+            assert len(outputs[i].outputs) == 1, f"Expected {1} outputs, but got {len(outputs[i].outputs)}"
+            response = outputs[i].outputs[0].text.strip()
+            correctness = extract_from_tags(remove_thinking_draft(response), "correctness")
+            res.append({
+                "idx": idxs[i],
+                "uuid": uuids[i],
+                "question": questions[i],
+                "answer": answers[i],
+                "ground_truth": ground_truths[i],
+                "correctness": correctness,
+            })
+
+        # save to jsonl file
+        with open(save_file, "a") as f:
+            for item in res:
+                f.write(json.dumps(item) + "\n")
